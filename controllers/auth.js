d6326f8a814244e8d19701807d864830a29d828b
longhuux <huulong140103@gmail.com>
2024-04-30 00:26:54 +0700

    30/4

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,103 @@
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const filterObj = require("../utils/filterObj");
+const otpGenerator = require("otp-generator");
 
-const signToken = (userId) => jwt.sign({userId}, process.env.JWT_SECRET)
+const signToken = (userId) => jwt.sign({ userId }, process.env.JWT_SECRET);
+
+exports.register = async (req, res, next) => {
+  const { firstName, lastName, email, password, verified } = req.body;
+  const filteredBody = filterObj(
+    req.body,
+    "firstName",
+    "lastName",
+    "password",
+    "email"
+  );
+
+  //check if a verified user with given email exist
+
+  const existing_user = await User.findOne({ email: email });
+
+  if (existing_user && existing_user.verified) {
+    res.status(400).json({
+      status: "error",
+      message: "Email is already in use, Please login",
+    });
+  } else if (existing_user) {
+    const updated_user = await User.findOneAndUpdate(
+      { email: email },
+      filteredBody,
+      { new: true, validateModifiedOnly: true }
+    );
+
+    //generate OTP and send email to user
+    req.userId = existing_user._id;
+    next();
+  } else {
+    //if user record is not available in DB
+    const new_user = await User.create(filteredBody);
+
+    //generate OTP and send email to user
+    req.userId = existing_user._id;
+    next();
+  }
+};
+
+exports.sendOTP = async (req, res, next) => {
+  const { userId } = req;
+  const new_otp = otpGenerator.generate(6, {
+    lowerCaseAlphabets: false,
+    upperCaseAlphabets: false,
+    specialChars: false,
+  });
+  const otp_expiry_time = Date.now() + 10 * 60 * 1000; //10mins after otp is sent
+
+  await User.findByIdAndUpdate(userId, {
+    otp: new_otp,
+    otp_expiry_time,
+  });
+  //TODO Send mail
+
+  res.status(200).json({
+    status: "success",
+    message: "OTP sent successfully",
+  });
+};
+
+exports.verifyOTP = async (req, res, next) => {
+  //verify OTP and update user record accordingly
+  const { email, otp } = req.body;
+  const user = await User.findOne({
+    email,
+    otp_expiry_time: { $gt: Date.now() },
+  });
+  if (!user) {
+    res.status(400).json({
+      status: "error",
+      message: "Email is invalid or otp expired",
+    });
+  }
+  if (!(await user.correctOTP(otp, user.otp))) {
+    res.status(400).json({
+      status: "error",
+      message: "OTP is incorrect",
+    });
+  }
+  //OTP is correct
+  user.verified = true;
+  user.otp = undefined;
+
+  await user.save({ new: true, validateModifiedOnly: true });
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: "success",
+    message: "OTP verified successfully",
+    token,
+  });
+};
 
 exports.login = async (req, res, next) => {
   const { email, password } = req.body;
@@ -23,11 +118,14 @@ exports.login = async (req, res, next) => {
     });
   }
 
-  const token = signToken(userId._id);
+  const token = signToken(userDoc._id);
   res.status(200).json({
     status: "success",
     message: "Logged in successfully",
-    token
-  })
-
+    token,
+  });
 };
+
+exports.forgotPassword = async (req, res, next) => {};
+
+exports.resetPassword = async (req, res, next) => {};
--- models/user.js
+++ models/user.js
@@ -45,14 +45,39 @@ const userSchema = new mongoose.Schema({
   updatedAt: {
     type: Date,
   },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  otp: {
+    type: Number,
+  },
+  otp_expiry_time: {
+    type: Date,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  //Only run this fn if otp is actually modified
+  if (!this.isModified("otp")) return next();
+  //Hash the otp with the cost of 12
+  this.otp = await bcrypt.hash(this.otp, 12);
+
+  next();
 });
 
 userSchema.methods.correctPassword = async function (
   candidatePassword,
-  userPassword
+  userPassword,
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
+userSchema.methods.correctOTP = async function (
+    candidateOTP,
+    userOTP,
+  ) {
+    return await bcrypt.compare(candidateOTP, userOTP);
+  };
 
 const User = new mongoose.model("User", userSchema);
 module.exports = User;
